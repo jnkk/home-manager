f3590241af77c105928adc63c84b0804d6e911df
jnkk <arian.reza.setiawan@gmail.com>
2024-10-06 16:52:34 +0700

    trying to add bash

--- flake.nix
+++ flake.nix
@@ -20,7 +20,10 @@
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [ ./home.nix ];
+        modules = [ 
+          ./home.nix 
+          ./shell/default.nix
+        ];
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
