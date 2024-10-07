960cfecadbe4a33cd9d117ca4e456e6e5f0fce61
jnkk <arian.reza.setiawan@gmail.com>
2024-10-07 09:02:21 +0700

    trying 'imports'

---
+++ shell/alias.nix
@@ -0,0 +1,30 @@
+{
+
+  programs.bash.shellAliases = {
+  	ls = "ls -al";
+  	mc = "micro";
+    bashrc = "micro .bashrc";
+    gs = "git status";
+    pg = "ping4 -i 5 google.com";
+    pacs = "apt search";
+    install = "sudo apt install";
+    "cd.." = "cd ..";
+    ".." = "cd ..";
+    sb = "source .bashrc";
+    up = "sudo apt update && sudo apt upgrade";
+    mi = "micro";
+    st = "speedtest-cli";
+    prt = "ping4 192.168.0.1";
+    wcc = "warp-cli connect";
+    wcd = "warp-cli disconnect";
+    wcs = "warp-cli status";
+    update = "sudo apt update";
+    upgrade = "sudo apt upgrade";
+    hmsf = "home-manager switch --flake .";
+
+
+
+
+  };
+
+}
