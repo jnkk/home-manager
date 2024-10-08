f1e43234709135294c90ec3a3f1123cfa1251f62
jnkk <arian.reza.setiawan@gmail.com>
2024-10-08 10:31:00 +0700

    adding direnv module

--- direnv/default.nix
+++ direnv/default.nix
@@ -1,5 +1,13 @@
 {
 
- 
+   programs = {
+    direnv = {
+      enable = true;
+      enableBashIntegration = true; # see note on other shells below
+      nix-direnv.enable = true;
+    };
+
+    bash.enable = true; # see note on other shells below
+  }; 
 
 }
